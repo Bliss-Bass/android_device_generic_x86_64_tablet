3b80d739d0409ea15f0b5c07090fc1ed364e4e1e
Jon West <electrikjesus@gmail.com>
2026-05-30 18:32:06 -0400

    Use chromeos fingerprint for google play

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -249,6 +249,9 @@ TARGET_RECOVERY_UI_LIB := librecovery_ui_x86
 TARGET_INIT_VENDOR_LIB ?= //$(LOCAL_COMMON_TREE):init_x86
 TARGET_RECOVERY_DEVICE_MODULES ?= init_x86
 
+# Fingerprint override
+BUILD_FINGERPRINT := google/kevin/kevin_cheets:7.1.1/R61-9765.85.0/4391409:user/release-keys
+
 # Include GloDroid components
 include $(LOCAL_COMMON_TREE)/glodroid/BoardConfig_glodroid.mk
 
